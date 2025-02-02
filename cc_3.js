6be08fd155549a7257fc868df60df9af58feadcb
Ashley Le <ashleyle@usf.edu>
2025-02-02 15:26:03 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,5 @@
+//Task 1: Product Price Management Scenario: Online Store You are managing a product pricing system.
+let prices = [1, 50, 97, 654521, 3]; //the prices yup
+prices.push(69); //adding a new price
+prices.shift(); //removing the first price
+console.log(`Updated Prices `, prices); //updated price list
